787925d4c6d687d135c76f3fb1310f0168e9614b
WorldJournal-Lyu <lyu@worldjournal.com>
2019-05-08 11:39:28 -0400

    Initial commit

---
+++ CheckMpaper.ps1
@@ -0,0 +1,80 @@
+<#
+
+CheckMpaper.ps1
+
+    2019-05-08 Initial Creation
+
+#>
+
+if (!($env:PSModulePath -match 'C:\\PowerShell\\_Modules')) {
+    $env:PSModulePath = $env:PSModulePath + ';C:\PowerShell\_Modules\'
+}
+
+Get-Module -ListAvailable WorldJournal.* | Remove-Module -Force
+Get-Module -ListAvailable WorldJournal.* | Import-Module -Force
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptName = (($MyInvocation.MyCommand) -Replace ".ps1")
+$hasError   = $false
+
+$newlog     = New-Log -Path $scriptPath -LogFormat yyyyMMdd-HHmmss
+$log        = $newlog.FullName
+$logPath    = $newlog.Directory
+
+$mailFrom   = (Get-WJEmail -Name noreply).MailAddress
+$mailPass   = (Get-WJEmail -Name noreply).Password
+$mailTo     = (Get-WJEmail -Name lyu).MailAddress
+$mailSbj    = $scriptName
+$mailMsg    = ""
+
+$localTemp = "C:\temp\" + $scriptName + "\"
+if (!(Test-Path($localTemp))) {New-Item $localTemp -Type Directory | Out-Null}
+
+Write-Log -Verb "LOG START" -Noun $log -Path $log -Type Long -Status Normal
+Write-Line -Length 50 -Path $log
+
+###################################################################################
+
+
+
+
+
+$ftp = Get-WJFTP -Name Oohla
+Write-Log -Verb "ftp" -Noun $ftp.Path -Path $log -Type Short -Status Normal
+
+
+
+
+
+
+
+###################################################################################
+
+Write-Line -Length 50 -Path $log
+
+# Delete temp folder
+
+Write-Log -Verb "REMOVE" -Noun $localTemp -Path $log -Type Long -Status Normal
+try{
+    $temp = $localTemp
+    Remove-Item $localTemp -Recurse -Force -ErrorAction Stop
+    Write-Log -Verb "REMOVE" -Noun $temp -Path $log -Type Long -Status Good
+}catch{
+    $mailMsg = $mailMsg + (Write-Log -Verb "REMOVE" -Noun $temp -Path $log -Type Long -Status Bad -Output String) + "`n"
+    $mailMsg = $mailMsg + (Write-Log -Verb "Exception" -Noun $_.Exception.Message -Path $log -Type Short -Status Bad -Output String) + "`n"
+}
+
+Write-Line -Length 50 -Path $log
+Write-Log -Verb "LOG END" -Noun $log -Path $log -Type Long -Status Normal
+if($hasError){ $mailSbj = "ERROR " + $mailSbj }
+
+$emailParam = @{
+    From    = $mailFrom
+    Pass    = $mailPass
+    To      = $mailTo
+    Subject = $mailSbj
+    Body    = $mailMsg
+    ScriptPath = $scriptPath
+    Attachment = $log
+}
+Emailv2 @emailParam
